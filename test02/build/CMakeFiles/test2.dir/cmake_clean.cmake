48477692790f8ede9a72b1097d2162dc8248aef5
Jean-Philippe Delisle <delisle_jp@outlook.com>
2014-01-29 21:14:32 -0500

    Tests

---
+++ test02/build/CMakeFiles/test2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test2.dir/main.cpp.o"
+  "test2.pdb"
+  "test2.exe"
+  "libtest2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/test2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
