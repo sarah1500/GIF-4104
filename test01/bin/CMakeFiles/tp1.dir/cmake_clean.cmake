86b53fc9ca8952609c97f03f1e0d005dd7ea3706
Jean-Philippe Delisle <delisle_jp@outlook.com>
2014-01-29 21:22:38 -0500

    Revert "Revert "Tests""
    This reverts commit 8b3a96d09638cf91e7a429bb10c80a006c819738.

---
+++ test01/bin/CMakeFiles/tp1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/tp1.dir/premiers.cpp.o"
+  "tp1.pdb"
+  "tp1.exe"
+  "libtp1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/tp1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
